c2a4792829fba8925a86b0809f9675396ff1a735
leonzug <63115665+leonzug@users.noreply.github.com>
2021-06-16 15:31:20 +0200

    Function to check if steady state is reached.

--- Utilities/is_steady_state.m
+++ Utilities/is_steady_state.m
@@ -1,9 +1,14 @@
-[isSteadyState,WhenSteadyState] = function(X,tol)
+function [isSteadyState,WhenSteadyState] = is_steady_state(X,tol)
 X_shifted = circshift ( X , [0,1]);
 dX = X-X_shifted;
 dX = dX(:,2:end);
-dX = sum(abs(dX),2);
-WhenSteadyState = 0;
-WhenSteadyState = find(dX < tol)(1);
-isSteadyState = WhenSteadyState >0;
+dX = sum(abs(dX),1);
+WhenSteadyState = find(dX < tol);
+if isempty(WhenSteadyState) == true
+    WhenSteadyState = 'never reached';
+    isSteadyState = 'False';
+    return
+end
+WhenSteadyState = WhenSteadyState(1);
+isSteadyState = 'True';
 end
