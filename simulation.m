a1fd1f8818c1bb27852e4d8962da37b6c8718198
suterz <suterzelio@gmail.com>
2021-06-16 10:03:35 +0200

    create kmeans function

--- simulation.m
+++ simulation.m
@@ -4,7 +4,7 @@ N = 100;
 timesteps = 100;
 
 traits = {'similarity', 'influenceable'};
-distr = {{'uniform',[0,1]}, {'uniform',[0,1]}}
+distr = {{'uniform',[0,1]}, {'uniform',[0,1]}};
 nRealNews = 3;
 nFakeNews = 3;
 step_size = 10;
@@ -18,17 +18,4 @@ eval = metrics (X,'mean', 10,2);
 
 visualize_function(A,X',nodenames,timesteps,1,step_size);
 
-X = X(:,end);
-opts = statset('Display','final');
-[idx,C] = kmeans(X,2,'Distance','cityblock',...
-    'Replicates',5,'Options',opts);
-figure(10);
-plot(X(idx==1,1),X(idx==1,1)*0,'r.','MarkerSize',12)
-hold on
-plot(X(idx==2,1),X(idx==2,1)*0,'b.','MarkerSize',12)
-plot(C(:,1),C(:,1)*0,'kx',...
-     'MarkerSize',15,'LineWidth',3) 
-legend('Cluster 1','Cluster 2','Centroids',...
-       'Location','NW')
-title 'Cluster Assignments and Centroids'
-hold off
+[idx, C] = k_means(X);
