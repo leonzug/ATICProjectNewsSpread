100edbbc8b210d600281e6d0bff89b13092c6be5
leonzug <63115665+leonzug@users.noreply.github.com>
2021-06-16 16:02:41 +0200

    Simulation file

--- simulation.m
+++ simulation.m
@@ -1,22 +1,31 @@
 addpath('Utilities');
 
-N = 30;
-timesteps = 2000;
+N = 6;
+timesteps = 100;
 
 traits = {'similarity', 'influenceable'};
 distr = {{'uniform',[0,1]}, {'uniform',[0,1]}};
-nRealNews = 3;
-nFakeNews = 1;
+nRealNews = 1;
+nFakeNews = 2;
 step_size = 10;
+tol = 0.0001;
 
 [A,people,FakeSources, RealSources,x0,nodenames] = generate_society (N,traits, distr, nRealNews, nFakeNews);
-
 [X] = spread_news(timesteps, A, x0);
+[isSteadyState,WhenSteadyState] = is_steady_state(X,tol);
+%eval = metrics (X,'hyst', 20,2);
+eval2 = metrics (X,'mean', 10,2);
 
-eval = metrics (X,'hyst', 20,2);
-eval2 = metrics (X,'mean', 10,2)
+degree = 2;
+perturbation = 'censorship';
+
+[A_perturbed] = perturb_network(A,FakeSources,RealSources,perturbation,degree)
+FakeSources
+RealSources
+A
+A_perturbed
 
 
-visualize_function(A,X',nodenames,timesteps,1,step_size);
+%visualize_function(A,X',nodenames,timesteps,1,step_size);
 
-[idx, C] = k_means(X);
+%[idx, C] = k_means(X);
