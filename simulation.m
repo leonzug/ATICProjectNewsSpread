566eb38ddcc7fac5a9d96e93502e820a44e1848f
leonzug <63115665+leonzug@users.noreply.github.com>
2021-06-16 18:16:08 +0200

    simulation.n for testing session

--- simulation.m
+++ simulation.m
@@ -1,15 +1,18 @@
 addpath('Utilities');
 
-N = 15;
-timesteps = 50;
+N = 100;
+timesteps = 600;
 
 traits = {'similarity', 'influenceable','critical thinker'};
-distr = {{'uniform',[0,1]}, {'uniform',[0,1]}, {'uniform',[0,1]}};
-nRealNews = 1;
-nFakeNews = 1;
+distr = {{'fixed',0.5}, {'fixed',0.5}, {'fixed',0.5}};
+nRealNews = 2;
+nFakeNews = 2;
 
-step_size = 10;
-tol = 0.0001;
+step_size = 100;
+tol = 0.000001;
+
+C = 0.2;
+nRoot = 4;
 
 % newsRange: how many people does a news reach. 
 % locality: is the news local or homogeneously spread.
@@ -17,17 +20,20 @@ tol = 0.0001;
 newsRange = round([0.1, 0.1]*N);
 locality = [true, true];
 
-[A,people,FakeSources, RealSources,x0,nodenames] = generate_society (N,traits, distr, nRealNews, nFakeNews, newsRange, locality);
+[A,people,FakeSources, RealSources,x0,nodenames] = generate_society (N,traits, distr, nRealNews, nFakeNews, newsRange, locality, C, nRoot);
 
 [X] = spread_news(timesteps, A, x0);
 [isSteadyState,WhenSteadyState] = is_steady_state(X,tol);
 %eval = metrics (X,'hyst', 20,2);
 eval2 = metrics (X,'mean', 10,2);
 
-degree = 2;
-perturbation = 'censorship';
+%degree = 2;
+%perturbation = 'censorship';
 
-
-%visualize_function(A,X',nodenames,timesteps,1,step_size);
+X_average = metrics(X, 'avg', 10, 2)
+X_average = X_average(end);
+[average_indegree] = visualize_function(A,X',nodenames,timesteps,1,step_size);
+%G = digraph(A,nodenames);
+%average_indegree = mean(indegree(G));
 
 %[idx, C] = k_means(X);
