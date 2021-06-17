d7895c050f31dc97e3ccc33b52261434ec629de1
leonzug <63115665+leonzug@users.noreply.github.com>
2021-06-17 11:08:11 +0200

    allow static and dynamic plotting of graph

--- Utilities/visualize_function.m
+++ Utilities/visualize_function.m
@@ -1,4 +1,4 @@
-function [average_indegree] = visualize_function(A,X,nodenames,duration,nfigure,step_size)
+function [average_indegree] = visualize_function(A,X,nodenames,duration,nfigure,step_size,dynamic)
     G = digraph(A,nodenames);
     average_indegree = mean(indegree(G));
     bins = conncomp(G);
@@ -7,18 +7,26 @@ function [average_indegree] = visualize_function(A,X,nodenames,duration,nfigure,
     %colorMap = [linspace(0.8,0,256)',linspace(0.8,0,256)',linspace(0.8,0,256)' ];
     %colormap(colorMap);
     colormap jet
-    for i = 1:size(X,1)
-        %hold on
+    if dynamic
+        for i = 1:size(X,1)
+            %hold on
 
-        if mod(i,step_size)==0
-            figure(nfigure)
-            p=plot(G,'EdgeLabel',round(G.Edges.Weight,1), 'Marker', 'o', 'MarkerSize', 12,'NodeCData',X(i,:),'NodeColor', 'flat'); 
-            title("iterazione: "+ i)
-            colorbar;
-            caxis([-1 1]);
-            pause(delay);
+            if mod(i,step_size)==0
+                figure(nfigure)
+                p=plot(G,'EdgeLabel',round(G.Edges.Weight,1), 'Marker', 'o', 'MarkerSize', 12,'NodeCData',X(i,:),'NodeColor', 'flat'); 
+                title("iterazione: "+ i)
+                colorbar;
+                caxis([-1 1]);
+                pause(delay);
+            end
+            %hold off
         end
-        %hold off
-    end
+    else
+        figure(nfigure)
+        p=plot(G,'EdgeLabel',round(G.Edges.Weight,1), 'Marker', 'o', 'MarkerSize', 12,'NodeCData',X(end,:),'NodeColor', 'flat'); 
+        title("Solution at final step")
+        colorbar;
+        caxis([-1 1]);
+        
 end
 
